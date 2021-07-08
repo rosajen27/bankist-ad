c4ae26f47e20e4aa69df8dc8cb2b8d3e5df7cd7a
Jennifer Rosa <rosajen27@gmail.com>
2021-07-08 12:45:18 -0400

    include cookies message

--- script.js
+++ script.js
@@ -31,3 +31,14 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Cookie Message
+const header = document.querySelector(".header");
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML = "We use cookies for improved functionality and analytics. <button class='btn btn--close--cookie'>Got it!</button>";
+header.append(message);
+
+document.querySelector(".btn--close--cookie").addEventListener("click", function() {
+	message.remove();
+});
