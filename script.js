b5d396a08dc2afdb1d5a1f3ed8d4bf51170442e4
Jennifer Rosa <rosajen27@gmail.com>
2021-07-09 11:54:59 -0400

    update cookie message style

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-'use strict';
+"use strict";
 
 ///////////////////////////////////////
 // Modal window
 
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.btn--close-modal');
-const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
 const openModal = function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
 
-btnsOpenModal.forEach(function(btn) {
+btnsOpenModal.forEach(function (btn) {
   btn.addEventListener("click", openModal);
 });
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
 });
 
+///////////////////////////////////////
 // Cookie Message
 const header = document.querySelector(".header");
 const message = document.createElement("div");
@@ -39,6 +40,9 @@ message.classList.add("cookie-message");
 message.innerHTML = "We use cookies for improved functionality and analytics. <button class='btn btn--close--cookie'>Got it!</button>";
 header.append(message);
 
-document.querySelector(".btn--close--cookie").addEventListener("click", function() {
-	message.remove();
+// Cookie styles
+message.style.backgroundColor = "#37383d";
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + "px";
+document.querySelector(".btn--close--cookie").addEventListener("click", function () {
+  message.remove();
 });
