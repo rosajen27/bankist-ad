0897cfa40066a92a14c6e75fbd52f7ad300ea7ad
Jennifer Rosa <rosajen27@gmail.com>
2021-07-14 09:31:29 -0400

    Page Navigation - smooth scrolling

--- script.js
+++ script.js
@@ -57,4 +57,15 @@ const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" });
-}); 
+});
+
+///////////////////////////////////////
+// Page Navigation - Smooth Scrolling
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    e.preventDefault();
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
