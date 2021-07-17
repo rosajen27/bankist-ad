b6329b1ab931a5b3330766d2a355dfeb1d073cbc
Jennifer Rosa <rosajen27@gmail.com>
2021-07-17 18:00:29 -0400

    Tabbed Components

--- script.js
+++ script.js
@@ -69,3 +69,31 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+
+///////////////////////////////////////
+// Tabbed Component
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+  const clicked = e.target.closest(".operations__tab");
+
+  if (clicked) {
+
+    // Remove active classes on tabs
+    tabs.forEach(function (t) {
+      t.classList.remove("operations__tab--active");
+    });
+    // Activate clicked tab
+    clicked.classList.add("operations__tab--active");
+  }
+
+  // Remove active classes on content
+  tabsContent.forEach(function (c) {
+    c.classList.remove("operations__content--active");
+  });
+  // Activate content area for clicked tab
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
+  
+});
