7a0206b578b7c9c3ae7ff6be0c4167fb4b30fc05
Jennifer Rosa <rosajen27@gmail.com>
2021-07-17 18:50:04 -0400

    Menu Fade Animation

--- script.js
+++ script.js
@@ -95,5 +95,39 @@ tabsContainer.addEventListener("click", function (e) {
   });
   // Activate content area for clicked tab
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
-  
+
+});
+
+///////////////////////////////////////
+// Menu Fade Animation
+const nav = document.querySelector(".nav");
+
+nav.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach(function (el) {
+      if (el !== link) {
+        el.style.opacity = 0.5;
+        logo.style.opacity = 0.5;
+      }
+    });
+  }
+});
+
+nav.addEventListener("mouseout", function (e) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    siblings.forEach(function (el) {
+      if (el !== link) {
+        el.style.opacity = 1;
+        logo.style.opacity = 1;
+      }
+    });
+  }
 });
