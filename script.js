3e763e15c7c2598d859c554c19b629906c42b60b
Jennifer Rosa <rosajen27@gmail.com>
2021-07-22 15:44:54 -0400

    Lazy Loading on images

--- script.js
+++ script.js
@@ -172,3 +172,33 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add("section--hidden");
 });
+
+///////////////////////////////////////
+// Lazy Loading
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    // Replace src with data-src
+    entry.target.src = entry.target.dataset.src;
+
+    // Remove blur filter once high resolution img is loaded
+    entry.target.addEventListener("load", function () {
+      entry.target.classList.remove("lazy-img");
+    });
+    observer.unobserve(entry.target);
+  }
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  // start loading images 200px before threshold
+  rootMargin: "200px",
+});
+
+imgTargets.forEach(function (img) {
+  imgObserver.observe(img);
+})
